5d7ad38e2186f96c717919303b8fa48940ae57c5
Giulianno Zanetti <giulianno.zanetti@grupomultiplica.com.br>
2022-04-22 13:53:41 -0300

    Concluindo Projeto de Repositorios

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,14 +3,22 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
 import { useState, useEffect } from "react";
 
+interface Repository{
+    name: string,
+    description: string,
+    html_url: string
+}
 
-export function RepositoryList(props){
-  const [repositories, setRepositories] = useState([]);
+export function RepositoryList(){
+  const [repositories, setRepositories] = useState<Repository[]> ([]);
 
   useEffect(() => {
     fetch("https://api.github.com/users/Giuzntt/repos")
       .then((response) => response.json())
-      .then((data) => setRepositories(data));
+      .then((data) => setRepositories(data)
+        
+        
+      );
   }, []);
 
     return (
